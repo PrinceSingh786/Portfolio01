41240036dd826b3fa420df02bc1cc7ba97a67ddd
Prince Singh <psjeee8@gmail.com>
2026-01-16 17:51:11 +0530

    animation change

--- src/Components/TextChange.jsx
+++ src/Components/TextChange.jsx
@@ -1,31 +1,29 @@
-import React from "react";
 import { useState, useEffect } from "react";
 const TextChange = () => {
-  const texts = ["Hi, I'm Prince Singh", "Hi, I'm Prince Singh", "Hi, I'm Prince Singh"];
+  const texts = "Hi, I'm Prince Singh";
   const [currenText, setCurrentText] = useState("");
   const [endValue, setendValue] = useState(true);
   const [isForward, setIsForward] = useState(true);
-  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentText(texts[index].substring(0, endValue));
+      setCurrentText(texts.substring(0, endValue));
       if (isForward) {
         setendValue((prev) => prev + 1);
       } else {
         setendValue((prev) => prev - 1);
       }
-      if (endValue > texts[index].length + 10) {
+      if (endValue==texts.length+1) {
         setIsForward(false);
       }
-      if (endValue < 2.1) {
+      if (endValue ==2) {
         setIsForward(true);
-        setIndex((prev) => prev & texts.length);
+        
       }
     }, 50);
 
     return () => clearInterval(intervalId);
-  }, [endValue, isForward, index, texts]);
+  }, [endValue, isForward, texts]);
 
   return <div className="transition ease duration-300">{currenText}</div>;
 };
